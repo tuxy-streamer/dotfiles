5d340b1491e58a88988ada1edb1c504f6dc0d1b9
penguineo <subham020304@gmail.com>
2025-04-22 20:36:28 +0530

    feat(nvim): add more formatter

--- .config/nvim/lua/penguineo/plugins/conform.lua
+++ .config/nvim/lua/penguineo/plugins/conform.lua
@@ -5,11 +5,13 @@ return {
 		require("conform").setup({
 			formatted_by_ft = {
 				lua = { "stylua" },
-				python = { "isort", "blackd", "ruff" },
+				python = { "ruff","isort", "blackd" },
 				rust = { "rustfmt", lsp_format = "fallback" },
 				javascript = { "prettierd", "eslintd" },
 				typescript = { "prettierd", "eslintd" },
-				go = { "gofmt" },
+				go = { "gofumpt", "goimports", "goimports-reviser", "golines", "gomodifytags", "gotests" },
+				yaml = { "yamlfix", "yamlfmt" },
+				bash = { "shfmt" },
 			},
 			format_on_save = function(bufnr)
 				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
