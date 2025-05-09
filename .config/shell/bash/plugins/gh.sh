ead02cc1bafb22d2cac27880384c6ab4ca494160
penguineo <subham020304@gmail.com>
2025-05-09 22:10:34 +0530

    fix(shell): using variables in double quotations

--- .config/shell/bash/plugins/gh.sh
+++ .config/shell/bash/plugins/gh.sh
@@ -1,20 +1,20 @@
 ghrc() {
 	repo=$(gh repo list | fzf | awk '{print $1}')
-	gh repo clone $repo
+	gh repo clone "$repo"
 }
 
 ghrd() {
 	repo=$(gh repo list | fzf | awk '{print $1}')
 	echo "$repo" | xclip -selection clipboard
-	gh repo delete $repo
+	gh repo delete "$repo"
 }
 
 ghra() {
 	repo=$(gh repo list | fzf | awk '{print $1}')
-	gh repo archive $repo
+	gh repo archive "$repo"
 }
 
 ghre() {
 	repo=$(gh repo list | fzf | awk '{print $1}')
-	gh repo edit $repo
+	gh repo edit "$repo"
 }
