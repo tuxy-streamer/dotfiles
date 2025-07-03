e26663d91164f18513806cc86510d47b87440f82
tuxy <subham020304@gmail.com>
2025-07-03 14:28:51 +0000

    refactor(shell): change dir for pnpm env

--- .config/shell/core/env.sh
+++ .config/shell/core/env.sh
@@ -1,10 +1,11 @@
+set -o vi
 export CONFIG="$HOME/.config"
 export STORAGE="/storage"
 export CORE="$CONFIG/shell/core"
 export BASH_PLUGINS="$CONFIG/shell/bash/plugins"
 
 # pnpm
-export PNPM_HOME="/home/penguineo/.local/share/pnpm"
+export PNPM_HOME="/home/tuxy/.local/share/pnpm"
 case ":$PATH:" in
 *":$PNPM_HOME:"*) ;;
 *) export PATH="$PNPM_HOME:$PATH" ;;
@@ -21,3 +22,6 @@ eval "$(starship init bash)"
 
 # clipmenu
 export CM_LAUNCHER=rofi
+
+# zoxide
+eval "$(zoxide init bash)"
