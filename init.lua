a80216189fe347888f5b50f4eeea8789f36d0a91
tuxy <subham020304@gmail.com>
2025-07-03 14:23:33 +0000

    feat: add globalconfig to init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
-require("penguineo.core.options")
-require("penguineo.core.lazy")
-require("penguineo.core.autocmd")
-require("penguineo.core.keymaps")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.autoformat = true
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+vim.g.deprecation_warnings = true
+vim.g.trouble_lualine = true
+vim.g.markdown_recommended_style = 0
+require("tuxy.core.lazy")
+require("tuxy.core.options")
+require("tuxy.core.autocmd")
+require("tuxy.core.keymaps")
