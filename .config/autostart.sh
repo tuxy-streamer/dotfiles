3e80bbfa5c6e5e8048d28655801603fc09774699
tuxy <subham020304@gmail.com>
2025-07-09 20:20:05 +0530

    feat: change autostart applications

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+tmux_startup() {
+	tmux new-session -ds temp_session && tmux run-shell "$HOME/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh" && tmux kill-session -t temp_session
+	alacritty --class=code -e tmux a &
+	alacritty --class=notes -e tmux a -t notes &
+}
+tmux_startup &
 picom -b &
-feh --bg-fill --borderless /storage/personal/wallpaper/girl-reading-book.png &
-systemctl --user enable --now opentabletdriver &
+dunst &
+feh --bg-fill --borderless /storage/personal/wallpaper/gruvbox/gruvbox-dark.png &
+brave &
+easyeffects &
+telegram-desktop &
+rnote &
