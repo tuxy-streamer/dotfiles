440064ed9db3ea98d51fcbeb0a95f7778e3b7d93
tuxy <subham020304@gmail.com>
2025-07-22 11:01:45 +0530

    feat(nvim): add mason-tool-installer plugin
    - Add linters and formatters for autoinstallation.

--- .config/nvim/lua/tuxy/plugins/lsp.lua
+++ .config/nvim/lua/tuxy/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = {
 		"mason-org/mason.nvim",
 		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	event = "BufReadPre",
 	config = function()
@@ -16,6 +17,62 @@ return {
 
 		require("fidget").setup({})
 		require("mason").setup()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				-- Assembly
+				"asmfmt",
+				-- Go
+				"gofumpt",
+				"golangci-lint",
+				"goimports-reviser",
+				"golines",
+				"gomodifytags",
+				"gotests",
+				-- Python
+				"pylint",
+				"pydocstyle",
+				"vulture",
+				-- Lua
+				"luacheck",
+				"stylua",
+				-- Git
+				"gitleaks",
+				"gitlint",
+				-- Rust
+				"codelldb",
+				-- JS/TS
+				"prettierd",
+				"eslint_d",
+				-- Yaml
+				"yamllint",
+				"yamlfix",
+				"yamlfmt",
+				-- Json
+				"jsonlint",
+				"fixjson",
+				-- Bash
+				"shellcheck",
+				"shellharden",
+				"shfmt",
+				-- Make
+				"checkmake",
+				-- Css
+				"stylelint",
+				-- Markdown
+				"markdownlint-cli2",
+				-- Html
+				"htmlhint",
+				-- C++
+				"clangtidy",
+				-- Sql
+				"sqlfluff",
+				"sqruff",
+				"pgformatter",
+			},
+			auto_update = true,
+			run_on_start = true,
+			debounce_hours = 10,
+		})
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"asm_lsp",
