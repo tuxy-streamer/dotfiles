409a7e578e42f176a5834bd0b41bd4b278cd6c0a
tuxy <subham020304@gmail.com>
2025-07-22 11:00:45 +0530

    fix(nvim): fixed typo
    - Added

--- .config/nvim/lua/tuxy/plugins/lsp.lua
+++ .config/nvim/lua/tuxy/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = {
-				"asm-lsp",
+				"asm_lsp",
 				"lua_ls",
 				"rust_analyzer",
 				"gopls",
