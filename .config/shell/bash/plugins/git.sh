75509af0a4e680689688a6bba8bdb9517a2f40f1
tuxy <subham020304@gmail.com>
2025-07-30 15:04:45 +0530

    refactor(shell): combine multiple echo commands into one

--- .config/shell/bash/plugins/git.sh
+++ .config/shell/bash/plugins/git.sh
@@ -6,8 +6,7 @@ gitb() {
 gitr() {
 	basebranch=$(git branch -l | sed 's/*/ /g' | fzf | sed 's/ //g')
 	rebasingbranch=$(git branch -l | sed "/$basebranch/d" | sed 's/*/ /g' | fzf | sed 's/ //g')
-	echo "Choose your base branch: $basebranch "
-	echo "Choose your rebasing branch: $rebasingbranch"
+	echo -e "Choose your base branch: $basebranch\nChoose your rebasing branch: $rebasingbranch"
 	echo -n "Do you want to continue rebasing ? (y/n) "
 	read -r rebase_input
 	[[ "$rebase_input" == 'Y' || "$rebase_input" == 'y' ]] && git checkout "$rebasingbranch" && git rebase "$basebranch" && echo "$rebasingbranch is rebased on $basebranch"
