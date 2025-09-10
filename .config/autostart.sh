d68bd1350ee9e2ae3c83ce3ae4a94ffb887827d6
tuxy <subham020304@gmail.com>
2025-09-10 14:49:17 +0530

    fix: removed apps that I don't use

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -2,14 +2,10 @@
 tmux_startup() {
 	tmux new-session -ds temp_session && tmux run-shell "$HOME/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh" && tmux kill-session -t temp_session
 	alacritty --class=code -e tmux a -t "$(awk 'END {print $2}' "$HOME/.config/tmux/resurrect-store/last")" &
-	alacritty --class=notes -e tmux a -t notes &
 }
 tmux_startup &
 dunst &
 feh --bg-fill --borderless /storage/personal/wallpaper/gruvbox/gruvbox-dark.png &
-waterfox &
+brave &
 easyeffects &
-Telegram &
-vesktop &
-rnote &
 setxkbmap -option caps:escape &
