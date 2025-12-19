0eba8cb21f7220d532e3c7b25a197f0e3010fbed
tuxy <subham020304@gmail.com>
2025-12-19 18:48:46 +0530

    feat(shell): add zsh plugins

---
+++ .config/shell/zsh-plugins/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+source "$ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$ZSH_PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$ZSH_PLUGINS/zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh"
