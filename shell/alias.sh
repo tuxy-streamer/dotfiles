694a7c18d5bb37e9b92c4db6fa91b6d9ded86f8f
tuxy-streamer <subham020304@gmail.com>
2026-02-17 23:08:22 +0530

    feat(shell): add package manager alias depend on os

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,2 +1,20 @@
 alias lah='ls -lah --group-directories-first --color=always'
 alias gr='cd /storage/projects/repos/$(ls /storage/projects/repos | fzf)'
+
+os=$(grep '^ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+case "$os" in
+  arch)
+    alias update-pkg="sudo pacman -Syu"
+    alias install-pkg="sudo pacman -Sy"
+    alias remove-pkg="sudo pacman -Rcns"
+    alias clean-pkg-cache="sudo pacman -Scc"
+    [[ $(command -v paru) ]] && \
+      alias aur-update="paru -Syu" && \
+      alias aur-install="paru -Sy" && \
+      alias aur-remove="paru -Rcns"
+      alias aur-clean-cache="paru -Scc"
+    ;;
+  *)
+    command ...
+    ;;
+esac
