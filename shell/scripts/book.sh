5ea6328714d542b7e2c6da2c89fd656fe021bdae
tuxy-streamer <subham020304@gmail.com>
2026-02-24 14:51:15 +0530

    refactor(shell): remove unnecessary code from book.sh

--- shell/scripts/book.sh
+++ shell/scripts/book.sh
@@ -1,24 +1,6 @@
-#!/usr/bin/env bash
-
-book_list=$(fd . -t file "$BOOK_LIBRARY")
-
-get_books() {
-    local -a book_names=()
-    local book
-
-    while IFS= read -r book; do
-        book="${book##*/}"
-        book="${book%.pdf}"
-        book_names+=("$book")
-    done <<<"$book_list"
-    printf "%s\n" "${book_names[@]}"
-}
-
 open_book() {
-    local selected
-    local book_path
+	books=$(find "$BOOK_LIBRARY" -type f  | sed 's|.*/||; s|\.pdf$||')
+	selected=$( echo "$books" | rofi -dmenu)
 
-    selected=$(get_books | rofi -dmenu)
-    
-    [[ -n "$selected" ]] && book_path=$(echo "$book_list" | rg "${selected}\.pdf$") && zathura "$book_path"
+	[[ -n "$selected" ]] && zathura "$(find "$BOOK_LIBRARY" -type f | grep "${selected}\.pdf$")"
 }
