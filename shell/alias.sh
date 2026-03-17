1a622874e79faa01d65db92438d879aa0939ce8f
tuxy-streamer <subham020304@gmail.com>
2026-03-17 23:45:18 +0530

    refactor(shell): package manager aliases

--- shell/alias.sh
+++ shell/alias.sh
@@ -9,20 +9,29 @@ case "$os" in
         alias r="sudo pacman -Rcns"
         alias c="sudo pacman -Scc"
         alias s="sudo pacman -Ss"
-        [[ $(command -v paru) ]] && \
-            alias auru="paru -Syu" && \
-            alias auri="paru -Sy" && \
-            alias aurre="paru -Rcns" &&\
-            alias aurc="paru -Scc" &&\
+        [[ $(command -v paru) ]] && {
+            alias auru="paru -Syu"
+            alias auri="paru -Sy"
+            alias aurre="paru -Rcns"
+            alias aurc="paru -Scc"
             alias aurs="paru -Ss"
-        [[ $(command -v yay) ]] && \
-            alias auru="yay -Syu" && \
-            alias auri="yay -Sy" && \
-            alias aurre="yay -Rcns" &&\
-            alias aurc="yay -Scc" &&\
+        }
+        [[ $(command -v yay) ]] && {
+            alias auru="yay -Syu"
+            alias auri="yay -Sy"
+            alias aurre="yay -Rcns"
+            alias aurc="yay -Scc"
             alias aurs="yay -Ss"
+        }
         ;;
+	fedora)
+		alias u="sudo dnf -y update"
+		alias i="sudo dnf -y install"
+		alias r="sudo dnf -y remove"
+		alias c="sudo dnf -y clean all"
+		alias s="sudo dnf search"
+		;;
     *)
-        command ...
+        echo "Unsupported OS: $os"
         ;;
 esac
