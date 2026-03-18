ad49ed9dc9adb4f52d8cc9ad61557dd57ed2c060
tuxy-streamer <subham020304@gmail.com>
2026-03-18 07:07:56 +0530

    refactor(shell): add logs to cd and add zoxide support

--- shell/scripts/autoswitch_env.sh
+++ shell/scripts/autoswitch_env.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 cd() {
-    builtin cd "$@" || return
-    [[ -d ".venv" ]] && [[ -z "$VIRTUAL_ENV" ]] && . .venv/bin/activate
-    [[ ! -d ".venv" ]] && [[ -n "$VIRTUAL_ENV" ]] && type deactivate >/dev/null 2>&1 && deactivate
+    builtin cd "$@" || return 1
+    [[ -d ".venv" ]] && [[ -z "$VIRTUAL_ENV" ]] && . .venv/bin/activate && echo "Activated python .venv"
+    [[ ! -d ".venv" ]] && [[ -n "$VIRTUAL_ENV" ]] && type deactivate >/dev/null 2>&1 && deactivate && echo "Deactivated python .venv"
+	return 0
+}
+
+z() {
+    local dir
+    dir=$(zoxide query "$@")
+	cd "$dir" || return 1
+	return
 }
